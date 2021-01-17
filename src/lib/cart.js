314ca9c2ced3d53b370f61783634178e72ed8736
Lúcio Nonato <39486464+N0N4T0@users.noreply.github.com>
2021-01-17 11:28:17 -0300

    15.1 commit
    adcionando items ao carrinho de compras

--- src/lib/cart.js
+++ src/lib/cart.js
@@ -6,12 +6,12 @@ const { formatPrice } = require('./utils')
 const Cart = {
     init(oldCart){
         if(oldCart){
-            this.items = oldCart.items
+            this.items = oldCart.items //[{ product: {}, price, quantity, formattedPrice}, {}]
             this.total = oldCart.total
         } else {
             this.items = []
             this.total = {
-                quantiy: 0,
+                quantity: 0,
                 price: 0,
                 formattedPrice: formatPrice(0)
             }
@@ -21,7 +21,40 @@ const Cart = {
     },
 
     //adcionar um item do carrinho
-    addOne(product){},
+    addOne(product){
+        // ver se o produto já existe no carrinho
+        let inCart = this.items.find(item => item.product.id == product.id)
+
+        if(!inCart){
+            inCart = {
+                product: {
+                    ...product,
+                    formattedPrice: formatPrice(product.price)
+                },
+                quantity: 0,
+                price: 0,
+                formattedPrice: formatPrice(0)
+            }
+
+            this.items.push(inCart)
+        }
+
+        //max quantity exceded
+        if(inCart.quantity >= product.quantity) return this
+
+        //ipdate item
+        inCart.quantity++
+        inCart.price = inCart.product.price * inCart.quantity
+        inCart.formattedPrice = formatPrice(inCart.price)
+
+
+        //update cart
+        this.total.quantity++
+        this.total.price += inCart.product.price
+        this.total.formattedPrice = formatPrice(this.total.price)
+
+        return this
+    },
 
 
     //remover 1 item do carrinho
@@ -31,5 +64,38 @@ const Cart = {
     //deletar todo o item
     delete(productId){}
 }
+
+const product = {
+    id: 1,
+    price: 199,
+    quantity: 2
+}
+
+const product2 = {
+    id: 2,
+    price: 229,
+    quantity: 1
+}
+
+//Estrutura
+// console.log(Cart.init().total)
+
+// Teste
+// console.log('add first cart item')
+// let oldCart = Cart.init().addOne(product)
+// console.log(oldCart)
+
+// console.log('add second cart item')
+// oldCart = Cart.init(oldCart).addOne(product)
+// console.log(oldCart)
+
+// console.log('add third cart item')
+// oldCart = Cart.init(oldCart).addOne(product2)
+// console.log(oldCart)
+
+// console.log('add last cart item')
+// oldCart = Cart.init(oldCart).addOne(product)
+// console.log(oldCart)
+
 
 module.exports = Cart
